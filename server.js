05ae8e901ed02abd99c5dc2b70bb67030ef8c901
Austin Moran <a.moran3257@gmail.com>
2019-04-09 10:21:54 -0600

    finished routes on server.js

--- server.js
+++ server.js
@@ -10,15 +10,49 @@ const knex = require('knex')(config)
 const port = 8000
 
 app.get('/', function(req, res, next){
-  res.send('get')
+  knex('restaurants')
+  .then((rows) => res.status(200).send(rows))
+  .catch(err => {
+    next(err)
+  })
 })
 
 app.post('/', function(req, res, next) {
-  res.send('post')
+  const username = req.body.username
+  const address = req.body.address
+  const city = req.body.city
+  const state = req.body.state
+  const geo_lat = req.body.geo_lat
+  const geo_long = req.body.geo_long
+  const distance = req.body.distance
+  const cache = req.body.cache
+
+  knex('users')
+  .insert({ username: username, address: address, city: city, state: state, geo_lat: geo_lat, geo_long: geo_long, distance: distance, cache: cache})
+  .then(() => res.status(200).send('user created'))
+  .catch(err => {
+    next(err)
+  })
 })
 
 app.patch('/', function(req, res, next) {
-  res.send('patch')
+  const id = req.body.id
+  const username = req.body.username
+  const address = req.body.address
+  const city = req.body.city
+  const state = req.body.state
+  const geo_lat = req.body.geo_lat
+  const geo_long = req.body.geo_long
+  const distance = req.body.distance
+  const cache = req.body.cache
+
+  knex('users')
+  .where({ id: id})
+  .update({address: address, city: city, state: state, geo_lat: geo_lat, geo_long: geo_long, distance: distance, cache: cache})
+  .then(() => res.status(200).send('update successful'))
+  .catch(err => {
+    next(err)
+  })
 })
 
 app.delete('/', function(req, res, next) {
